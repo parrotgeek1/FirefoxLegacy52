40584a2c24125fb56ed41b527ba13df3d5711c52
parrotgeek1 <parrotgeek1@gmail.com>
2019-10-31 22:07:57 -0700

    Revert "Bug 1394654 - Remove OS X opengl workaround from Mozilla bug 603134. r=mstange, a=ritu"
    This reverts commit dd7613396bc77d1f50e9e085b5bc8eda9b6d38d6.

--- widget/cocoa/nsChildView.h
+++ widget/cocoa/nsChildView.h
@@ -244,6 +244,7 @@ typedef NSInteger NSEventGestureAxis;
   float mCumulativeMagnification;
   float mCumulativeRotation;
 
+  BOOL mDidForceRefreshOpenGL;
   BOOL mWaitingForPaint;
 
 #ifdef __LP64__
