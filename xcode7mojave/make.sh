1a42b4fa432fa5e2edc4210d1a3bf80019e32cf8
parrotgeek1 <parrotgeek1@gmail.com>
2020-04-02 21:03:22 -0500

    rename shim, put it inside xcode

--- xcode7mojave/make.sh
+++ xcode7mojave/make.sh
@@ -1,4 +1,5 @@
-clang -fPIC -O3 -Wall -Wextra -Werror -Wno-unused-parameter -arch x86_64 -dynamiclib -mmacosx-version-min=10.8 -Wl,-reexport_library,/System/Library/Frameworks/AppKit.framework/Versions/C/AppKit -current_version 1404.11 -compatibility_version 45 -o cursed2.dylib cursed.c
-perl -pi -e 's/IvarsA/Ivars./g' cursed2.dylib
-perl -pi -e 's/SharedDataA/SharedData./g' cursed2.dylib
-sudo cp cursed2.dylib /
+#!/bin/sh
+clang -fPIC -O3 -Wall -Wextra -Werror -Wno-unused-parameter -arch x86_64 -dynamiclib -mmacosx-version-min=10.8 -Wl,-reexport_library,/System/Library/Frameworks/AppKit.framework/Versions/C/AppKit -current_version 1404.11 -compatibility_version 45 -o shim.dylib shim.c
+perl -pi -e 's/IvarsA/Ivars./g' shim.dylib
+perl -pi -e 's/SharedDataA/SharedData./g' shim.dylib
+sudo cp shim.dylib /Applications/Xcode7.app/Contents/SharedFrameworks/DVTKit.framework/Versions/A/shim.dylib
--- xcode7mojave/patch.sh
+++ xcode7mojave/patch.sh
@@ -1,4 +1,6 @@
-sudo install_name_tool -change  /System/Library/Frameworks/AppKit.framework/Versions/C/AppKit '/cursed2.dylib' /Applications/Xcode7.app/Contents/SharedFrameworks/DVTKit.framework/Versions/A/DVTKit
+#!/bin/sh
+./make.sh
+sudo install_name_tool -change  /System/Library/Frameworks/AppKit.framework/Versions/C/AppKit '@loader_path/shim.dylib' /Applications/Xcode7.app/Contents/SharedFrameworks/DVTKit.framework/Versions/A/DVTKit
 sudo "./unsign/unsign" /Applications/Xcode7.app/Contents/SharedFrameworks/DVTKit.framework/Versions/A/DVTKit 
 sudo mv /Applications/Xcode7.app/Contents/SharedFrameworks/DVTKit.framework/Versions/A/DVTKit.unsigned /Applications/Xcode7.app/Contents/SharedFrameworks/DVTKit.framework/Versions/A/DVTKit
 sudo chmod +x /Applications/Xcode7.app/Contents/SharedFrameworks/DVTKit.framework/Versions/A/DVTKit
